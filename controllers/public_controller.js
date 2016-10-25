c88cf3a0b714e8f5453684da246623d020927958
Sergio Saldarriaga <sergio.saldarriaga88@gmail.com>
2016-10-24 21:13:30 -0400

    Updates public and style

--- controllers/public_controller.js
+++ controllers/public_controller.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
-
+router.get('/', function (req, res) {
+ res.render("home", {});
 });
 
-router.get('/login', function(req, res) {
+router.get('/login', function (req, res) {
 
 });
 
