898a7483d9afeb78dda4220c2f9626f6a88db2c5
sergsal <sergio.saldarriaga88@gmail.com>
2016-10-28 13:51:15 -0400

    Adds delete and reserve functionality to profile page

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,13 +1,60 @@
-//converts time in db to a moment.js formatted time
-$(".availability-time").each(function(i) {
- var time = $(this).text();
- var timeFormatted = moment(time).format("dddd, MMMM Do YYYY, hh:mm a");
- $(this).html(timeFormatted)
-});
-
 $(document).ready(function () {
-	// create DateTimePicker from input HTML element
-	$(".datetimepicker").kendoDateTimePicker({
-		value: new Date()
+	//converts time in db to a moment.js formatted time
+	$(".availability-time").each(function (i) {
+		var time = $(this).text();
+		var timeFormatted = moment(time).format("dddd, MMMM Do YYYY, hh:mm a");
+		$(this).html(timeFormatted)
 	});
+
+
+	function startChange() {
+		var startDate = start.value(),
+			endDate = end.value();
+
+		if (startDate) {
+			startDate = new Date(startDate);
+			startDate.setDate(startDate.getDate());
+			end.min(startDate);
+		} else if (endDate) {
+			start.max(new Date(endDate));
+		} else {
+			endDate = new Date();
+			start.max(endDate);
+			end.min(endDate);
+		}
+	}
+
+	function endChange() {
+		var endDate = end.value(),
+			startDate = start.value();
+
+		if (endDate) {
+			endDate = new Date(endDate);
+			endDate.setDate(endDate.getDate());
+			start.max(endDate);
+		} else if (startDate) {
+			end.min(new Date(startDate));
+		} else {
+			endDate = new Date();
+			start.max(endDate);
+			end.min(endDate);
+		}
+	}
+
+	var today = kendo.date.today();
+
+	var start = $("#start").kendoDateTimePicker({
+		value: today,
+		change: startChange,
+		parseFormats: ["MM/dd/yyyy"]
+	}).data("kendoDateTimePicker");
+
+	var end = $("#end").kendoDateTimePicker({
+		value: today,
+		change: endChange,
+		parseFormats: ["MM/dd/yyyy"]
+	}).data("kendoDateTimePicker");
+
+	start.max(end.value());
+	end.min(start.value());
 });
