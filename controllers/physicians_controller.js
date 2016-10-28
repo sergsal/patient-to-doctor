d47499a6f2a97c4ecfc50c22104c973c8f731ed6
Will McAuliff <womcauliff@gmail.com>
2016-10-28 12:19:54 -0400

    Creates and updates new physician with prof_pic via registration form.

--- controllers/physicians_controller.js
+++ controllers/physicians_controller.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var tokenAuth = require('./auth.js');
-var crypto = require('crypto');
-var mime = require('mime');
 
 //for image upload
+var crypto = require('crypto');
+var mime = require('mime');
 var multer = require('multer');
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, './public/profiles/physician/');
   },
+  //randomizing filename
   filename: function (req, file, cb) {
     crypto.pseudoRandomBytes(16, function (err, raw) {
       cb(null, raw.toString('hex') + Date.now() + '.' + mime.extension(file.mimetype));
@@ -46,37 +47,55 @@ router.get('/', function (req, res) {
   });
 });
 
+/**
+ * Route: `/physicians/register`
+ * Renders the handlebars view for the signup form.
+ * Clicking the submit button posts form data to `/physicians/create`
+ */
 router.get('/register', function(req, res){
   res.render("physician-register");
-  //renders the signup form handlebars view
-  //clicking the submit button posts form data to `/physicians/create`
 });
 
-//add new physician profile
+
+/**
+ * Route: `/physicians/create`
+ * Handles image upload and creates new physician profile
+ */
 router.post(
   '/create', 
-  upload.single('prof_pic'), 
+  upload.single('dr_prof_pic'), 
   function (req, res, next) {
-    console.log('file info: ', req.file);
-    console.log(req.body.specialty);
-    // Physician.create({
-    //   first_name: req.body.first_name,
-    //   last_name: req.body.last_name,
-    //   address1: req.body.address1,
-    //   address2: req.body.address2,
-    //   city: req.body.city,
-    //   state: req.body.state,
-    //   zip: req.body.zip,
-    //   phone_number: req.body.phone_number,
-    //   specialty: req.body.specialty,
-    //   prof_pic: req.files[0]
-    // })
-    // .then(function(newPhysician) {
-      console.log('received');
-      var newPhysician = {url_path: "howser1"};
-      //res.redirect('/dr/' + newPhysician.url_path);
-      res.redirect('/profiles/physician/' + req.file.filename);
-    // });
+
+    Physician.create({
+      first_name: req.body.dr_first_name,
+      last_name: req.body.dr_last_name,
+      address1: req.body.dr_address1,
+      address2: req.body.dr_address2,
+      city: req.body.dr_city,
+      state: req.body.dr_state,
+      zip: req.body.dr_zip,
+      phone_number: req.body.dr_phone_number,
+      specialty: req.body.dr_specialty,
+      prof_pic: req.file.filename
+    })
+    .then(function(newPhysician) {
+      console.log("updating physician..");
+      return Physician.update(
+        //set clause
+        {
+          url_path: newPhysician.last_name.toLowerCase() + newPhysician.id
+        },
+        //where clause
+        {
+          where: {
+            id: newPhysician.id
+          }
+        }
+      );
+    }).then(function(updatedPhysician) {
+      console.log("redirecting..");
+      res.redirect('/dr/' + updatedPhysician.url_path);
+    });
   }
 );
 
