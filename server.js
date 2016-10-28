2bfe5bcab7344246dab3620d3821671d0ddaaa39
sergsal <sergio.saldarriaga88@gmail.com>
2016-10-28 14:08:21 -0400

    Updates server settings

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ app.use('/physicians', physicians_routes);
 app.use('/search', search_routes);
 
 // Listening
-app.listen(app.get('port'), function () {
-	console.log('App listening on PORT ' + app.get('port'));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
